daed9a823d06000856a7aa7985ee48cdae816a51
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-10 15:21:05 +0545

    whack-a-mole-v1.0 completed

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 let score = 0;
+let timing = 30;
+let isStarted = false;
+
+const holes = document.querySelectorAll(".hole");
+let usrScore = document.getElementById("score");
+let countDownTime = document.getElementById("countdown-time");
+
+document.addEventListener("keydown", startFun);
+
+function startFun(e) {
+  if (e.key === " " && !isStarted) {
+    isStarted = true;
+    // console.log("he");
+    start();
+  }
+}
+
+function start() {
+  let myInterval = setInterval(() => {
+    let rndNum1 = Math.floor(Math.random() * 9);
+    let rndNum2 = Math.floor(Math.random() * 9);
+
+    holes[rndNum1].classList.add("mole");
+    holes[rndNum2].classList.add("mole");
+    //   console.log(`interval ${rndNum1} and ${rndNum2}`);
+    setTimeout(() => {
+      holes[rndNum1].classList.remove("mole");
+      holes[rndNum2].classList.remove("mole");
+      // console.log(`setTimeOut ${rndNum1} and ${rndNum2}`);
+    }, 1500);
+  }, 1500);
+
+  let timeInterval = setInterval(() => {
+    countDownTime.innerText = timing > 9 ? `00:${timing}` : `00:0${timing}`;
+    if (timing === 0) {
+      clearInterval(myInterval);
+      clearInterval(timeInterval);
+      timing = 30;
+      score = 0;
+      isStarted = false;
+    }
+    --timing;
+  }, 1000);
+}
+
+for (let i = 0; i < 9; i++) {
+  document.querySelectorAll(".hole")[i].addEventListener("click", holeClickFun);
+}
+
+function holeClickFun(e) {
+  if (e.target.classList.contains("mole")) {
+    score++;
+  }
+  usrScore.innerText = score;
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Whack A Mole
+
+[Click Here](https://)
+
+## Instruction to Users
+
+- Hit space to start playing.
+- Whack the mole where it is seen.
+- Try to whack as much as mole within time.
+
+## For developers
+
+Whack A Mole v1.0
+
+- A simple usable game is developed.
+
+<details>
+<summary>Current Issues</summary>
+    - Need to check CSS; it shows scroll bar even if used in full screen.
+    - Try to build better logic to randomized the whack in hole.
+</details>
